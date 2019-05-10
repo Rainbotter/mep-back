cd043d74485f935c59e59f2dd64195b69ffdef91
antoine <antoine.robert57@icloud.com>
2019-05-10 12:31:57 +0200

    test travis build script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo "Building docker image"
   docker --version
   docker build -t rainbowloutre/mep-back .
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
   docker push rainbowloutre/mep-back;
+else
+  echo "Nothing to build as this is not the master branch"
 fi
