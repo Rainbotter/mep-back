73f9abb2cb2596057beef35efe499acf178792e1
antoine <antoine.robert57@icloud.com>
2019-05-17 21:49:42 +0200

    add missing annotations in NotificationsController

--- src/main/java/lu/mypost/mep/controller/NotificationController.java
+++ src/main/java/lu/mypost/mep/controller/NotificationController.java
@@ -6,10 +6,14 @@ import lu.mypost.mep.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@RestController
+@RequestMapping("/notifications")
 public class NotificationController {
 
     private NotificationService notificationService;
