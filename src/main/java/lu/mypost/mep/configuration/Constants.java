c3b4080edbc6ca8cfbdd32d376bf5a7152dba243
antoine <antoine.robert57@icloud.com>
2019-08-28 19:27:49 +0200

    fix json date format

--- src/main/java/lu/mypost/mep/configuration/Constants.java
+++ src/main/java/lu/mypost/mep/configuration/Constants.java
@@ -2,7 +2,7 @@ package lu.mypost.mep.configuration;
 
 public class Constants {
 
-    public static final String dateSerializationFormat = "yyyy/MM/dd_HH:mm";
+    public static final String dateSerializationFormat = "yyyy-MM-dd'T'HH:mm:ssZ";
 
     public class Profiles {
         public static final String DEVELOPMENT = "development";
